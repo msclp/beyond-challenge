26dbd8810f389768508c49eba34a78dc72a364cd
Łukasz Pałasz <dev@palasz.io>
2023-02-17 15:48:49 +0100

    Create  API endpoint

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,10 +1,26 @@
 import express from 'express'
 import cors from 'cors'
+import path from 'path'
+import csv from 'csvtojson'
 
 const port = 4000
 
 const app = express()
 app.use(cors())
+
+app.get('/influencers', async (_, res, next) => {
+  const filePath = path.resolve(__dirname, '../data/influencers.csv')
+  try {
+    const data = await csv()
+      .subscribe(obj => {
+        obj.id = obj['Influencer insta name']
+      })
+      .fromFile(filePath)
+    res.json(data)
+  } catch (err) {
+    next(err)
+  }
+})
 
 app.get('/', (_, res) => {
   res.json({ status: 'ok' })
